7454432cc32a6aa84997371d82e8a25b70f6789f
Tejas Gorde <tejasgorde5@gmail.com>
2023-06-06 07:56:26 +0530

    06/06/2023

---
+++ Even or Odd Number.py
@@ -0,0 +1,7 @@
+#Write a program to check whether a number is even or odd using conditional operator.
+
+x=int(input("Enter the Number  :"))
+if x%2==0:
+    print(x,"is Even Number")
+else:
+    print(x,"is Odd Number")
