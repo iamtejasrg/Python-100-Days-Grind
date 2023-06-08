c3aaa346691cb027d1d6e93bde19a8ba7a7771df
Tejas Gorde <tejasgorde5@gmail.com>
2023-06-08 08:24:01 +0530

    08/06/2023

---
+++ multipleinput.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 13:07:46 2020
+
+@author: Dell
+"""
+
+# for two numbers input
+num3,num1, num2 =map(int, input().split())
+sum=num1+num2+num3
+print (sum)
