347b54d484283cbe1fdebf630ea80ac892dd74a8
Tejas Gorde <95203031+iamtejasrg@users.noreply.github.com>
2023-06-17 08:10:25 +0530

    Sum.py

--- sum.py
+++ sum.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 21 16:00:20 2020
-
-@author: Dell
-"""
-
 #Add the two numbers with map functions
 'enter the inputs'
 numArray=map(int, input('enter the numbers,').split())
